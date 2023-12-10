7a5152be8db9e95a69ebf4c21201520ee9e0fde2
Jessada <113906747+JessadaID@users.noreply.github.com>
2023-12-10 15:26:37 +0700

    Create week1-3.c

---
+++ week1/week1-3.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+int *GetSet( int *num ) ;
+
+int main() {
+ int *data, num,i ;
+ data = GetSet( &num ) ;
+
+ return 0 ;
+}//end function
+
+int *GetSet( int *num ){
+	int i;
+	printf("\ninput your size set : ");
+	scanf("%d",num);
+	int set[*num];
+	for (i = 0;i < *num ; i++){
+		printf("\ninput your value of set : ");
+		scanf("%d",&set[i]);
+	}
+	
+	for (i = 0;i < *num ; i++){
+		printf("\n%d <== from getset",set[i]);
+	}
+	return set;
+}
