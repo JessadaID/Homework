50366edbe4f01a4b1be64b4f9440834ba71262ab
Jessada <113906747+JessadaID@users.noreply.github.com>
2023-12-10 17:35:26 +0700

    Update week1-5.c

--- week1/week1-5.c
+++ week1/week1-5.c
@@ -1,22 +1,27 @@
-#include <stdio.h>
 
-void GetMatrix( int *value[], int *row, int *col ) ;
-
-int main() {
- int *data, m, n ;
- GetMatrix( &data, &m, &n ) ;
- return 0 ;
-}//end function
-
-void GetMatrix( int *value[], int *row, int *col ){
+void GetMatrix( int **value, int *row, int *col ){
 	printf("How many row,col ? : ");
 	scanf("%d %d",row,col);
 	
 	*value = new int [*row * *col];
+	
+	//printf("%d",value);
+	
+	
 	for(int i = 0 ; i < *row ; i++){
 		for(int j = 0 ; j < *col ; j++){
-			printf("value [%d][%d] ",i,j);
-			scanf("%d",&(*value)[i * *col * j]);
+			printf("value [%d][%d] \n",i,j);
+			scanf("%d",&(*value)[i * *col + j]);
 		}	
 	}
+	
+	printf("\n ==========================\n");
+	
+	for(int i = 0 ; i < *row ; i++){
+		for(int j = 0 ; j < *col ; j++){
+			printf("[%d]",(*value)[i * *col + j]);
+		}
+		printf("\n");	
+	}
+	
 }
