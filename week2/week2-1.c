2ded0e226c075edf64c0d83e21017ce8743425a5
Jessada <113906747+JessadaID@users.noreply.github.com>
2023-12-16 15:47:13 +0700

    Create week2-1.c

---
+++ week2/week2-1.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include "string.h"
+
+void reverse( char str1[], char str2[] ) ;
+
+int main() {
+ char text[ 50 ] = "Hello World" ;
+ char out[ 50 ] ;
+ reverse( text, out ) ;
+}//end function
+
+void reverse( char str1[], char str2[] ){
+	printf("%s\n",str1);
+	
+	for(int i = strlen(str1) ; i >= 0 ;i--){
+		printf("%c",str1[i-1]);
+	}
+	
+}
