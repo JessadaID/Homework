499bd06476462fc437d8b1ff8aca4369d6d68de3
Jessada <113906747+JessadaID@users.noreply.github.com>
2023-12-17 18:20:16 +0700

    Update week1-3.c

--- week1/week1-3.c
+++ week1/week1-3.c
@@ -13,14 +13,14 @@ int *GetSet( int *num ){
 	int i;
 	printf("\ninput your size set : ");
 	scanf("%d",num);
-	int set[*num];
+	int set[ *num ];
 	for (i = 0;i < *num ; i++){
 		printf("\ninput your value of set : ");
-		scanf("%d",&set[i]);
+		scanf("%d", &set[ i ]);
 	}
 	
 	for (i = 0;i < *num ; i++){
-		printf("\n%d <== from getset",set[i]);
+		printf("\n%d <== from getset", set[ i ]);
 	}
 	return set;
 }
