dc7a3919ca1ad9f70cc8c7a3ac435bbdc3706230
Jessada <113906747+JessadaID@users.noreply.github.com>
2023-12-17 18:19:03 +0700

    Update week1-1.c

--- week1/week1-1.c
+++ week1/week1-1.c
@@ -3,7 +3,7 @@
 int main(){
 	int input = 0;
 	scanf("%d",&input);
-	for(int i =0;i<input;i++){
-		printf("[%d] Hello World\n",i+1);
+	for(int i = 0 ; i < input ; i++){
+		printf("[%d] Hello World\n", i + 1);
 	}
 }//end function
