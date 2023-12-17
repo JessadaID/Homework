6f7443813de90151c53ad053f887a88a01c8937c
Jessada <113906747+JessadaID@users.noreply.github.com>
2023-12-17 18:21:00 +0700

    Update week1-4.c

--- week1/week1-4.c
+++ week1/week1-4.c
@@ -12,14 +12,14 @@ int main() {
 int *GetSet( int *arr[] ){
 	int i,num;
 	printf("\ninput your number of member : ");
-	scanf("%d",&num);
-	int set[num];
-	for (i = 0;i < num ; i++){
+	scanf( "%d" , &num);
+	int set[ num ];
+	for (i = 0 ; i < num ; i++){
 		printf("\ninput your value of member : ");
-		scanf("%d",&arr[i]);
+		scanf( "%d" , &arr[ i ]);
 	}
 	
 	for (i = 0;i < num ; i++){
-		printf("\n%d <== from GetSet",arr[i]);
+		printf("\n%d <== from GetSet", arr[ i ]);
 	}
 }
