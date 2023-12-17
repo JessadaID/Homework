dff477ad130f210e92eaea33fe0998172af1deed
Jessada <113906747+JessadaID@users.noreply.github.com>
2023-12-17 18:25:25 +0700

    Update week2-1.c

--- week2/week2-1.c
+++ week2/week2-1.c
@@ -10,9 +10,9 @@ int main() {
 }//end function
 
 void reverse( char str1[], char str2[] ){
-	printf("%s\n",str1);
-	for(int i = strlen(str1) ,j = 0 ; i >= 0; i-- , j++){
-		str2[j] = str1[i-1];
-	}
-	printf("%s",str2);
-}
+	printf("%s\n", str1);
+	for(int i = strlen( str1 ) , j = 0 ; i >= 0 ; i-- , j++){
+		str2[ j ] = str1[ i - 1 ];
+	}//end for
+	printf("%s", str2);
+}//end function
